5e841ef2ec5795f59e5400750e2eef26fefa2f5f
falconraptor <falconraptor@users.noreply.github.com>
2017-07-01 01:51:29 -0700

    reformat

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
+from datetime import datetime, timedelta
+from threading import Timer
+
 from discord.ext import commands
-from discord import Server, ChannelType, Client, Channel,Role
+from discord import Server, ChannelType, Client, Channel, Role
 
 from discord.utils import get
+
 
 class GroupInfo:
     def __init__(self):
         self.text = None
         self.voice = None
-        self.role = None      
+        self.role = None
+
 
 class Commands:
-    
-
     def __init__(self, bot):
         self.bot = bot
         self.groups = []
@@ -24,35 +27,59 @@ class Commands:
     async def create(self, ctx, name):
         author = ctx.message.author
         server = author.server
-        group = await create_groupinfo(self.bot,server,name)
+        group = await create_groupinfo(self.bot, server, name)
         self.groups.append(group)
         await self.bot.add_roles(author, group.role)
         await self.bot.move_member(author, group.voice)
 
+
 async def create_role(client: Client, server: Server, name: str):
-    return await client.create_role(server, name = name)
+    return await client.create_role(server, name=name)
 
 
-async def create_channel(client: Client, server: Server, name: str, is_voice: bool=False):
+async def create_channel(client: Client, server: Server, name: str, is_voice: bool = False):
     channel_type = ChannelType.text
     if is_voice:
         channel_type = ChannelType.voice
     return await client.create_channel(server, name, type=channel_type)
 
-async def remove_role(client: Client, server:Server, role:Role):
-    await client.delete_role(server,get(server.roles,id=role.id))
 
-async def remove_channel(client: Client, server:Server, channel:Channel):
-    await client.delete_channel(get(server.channels,id=channel.id))
-    
-async def create_groupinfo(client:Client,server:Server,name:str):
+async def remove_role(client: Client, server: Server, role: Role):
+    await client.delete_role(server, get(server.roles, id=role.id))
+
+
+async def remove_channel(client: Client, server: Server, channel: Channel):
+    await client.delete_channel(get(server.channels, id=channel.id))
+
+
+async def create_groupinfo(client: Client, server: Server, name: str):
     info = GroupInfo()
-    info.text = await create_channel(client,server,name)
-    info.voice = await create_channel(client,server,name,True)
-    info.role = await create_role(client,server,name)
+    info.text = await create_channel(client, server, name)
+    info.voice = await create_channel(client, server, name, True)
+    info.role = await create_role(client, server, name)
     return info
 
-async def remove_groupinfo(client:Client,server:Server,info:GroupInfo):
-    await remove_channel(client,server,info.text)
-    await remove_channel(client,server,info.voice)
-    await remove_role(client,server,info.role)
+
+async def remove_groupinfo(client: Client, server: Server, info: GroupInfo):
+    await remove_channel(client, server, info.text)
+    await remove_channel(client, server, info.voice)
+    await remove_role(client, server, info.role)
+
+
+timer = None
+first = True
+
+
+def run_timer():
+    global first, timer
+    today = datetime.now()
+    delta = (today.replace(minute=0) - today).total_seconds()
+    print('timers: {}'.format(delta))
+    if delta > 0:
+        if not first:
+            # your method call goes here
+            timer = None
+        if not timer:
+            timer = Timer(delta, run_timer)
+            # timer.start()
+        first = False
