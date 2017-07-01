85d6851564a1ebde8ff3c1e54dfb2ec611b83cfd
falconraptor <falconraptor@users.noreply.github.com>
2017-07-01 01:10:57 -0700

    reformat

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord import Server, ChannelType, Client
 from discord.utils import get
 
+
 class Commands:
     def __init__(self, bot):
         self.bot = bot
@@ -14,25 +15,23 @@ class Commands:
 
     @group.command(pass_context=True, no_pm=True)
     async def create(self, ctx, name):
-        
         role = await create_role(self.bot, ctx.message.author.server, name)
         await self.bot.add_roles(ctx.message.author, role)  # add role to user
-        self.roles += role  # add role to cache
+        self.roles.append(role)  # add role to cache
 
-        self.channels['text'] += await create_channel(self.bot, ctx.message.author.server, name)
+        self.channels['text'].append(await create_channel(self.bot, ctx.message.author.server, name))
 
-        vChannel = await create_channel(self.bot, ctx.message.author.server, name, True)
-        await self.bot.move_member(ctx.message.author, vChannel)  # move user to channel
-        self.channels['voice'] += vChannel  # add role to cache
+        v_channel = await create_channel(self.bot, ctx.message.author.server, name, True)
+        await self.bot.move_member(ctx.message.author, v_channel)  # move user to channel
+        self.channels['voice'].append(v_channel)  # add role to cache
+
 
 async def create_role(client: Client, server: Server, name: str):
     uncached = await client.create_role(server, name=name)
-    #return [r for r in server.roles if r.id == uncached.id][0] # returns
-    #cached role
-    temp = get(server.roles,id = uncached.id)
-    return temp
+    return get(server.roles, id=uncached.id)
 
-async def create_channel(client: Client, server: Server, name: str, is_voice: bool=False):
+
+async def create_channel(client: Client, server: Server, name: str, is_voice: bool = False):
     channel_type = ChannelType.text
     if is_voice:
         channel_type = ChannelType.voice
