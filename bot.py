9a6c67ec29ffae350cab0fab0491d65dcb3b377a
falconraptor <falconraptor@users.noreply.github.com>
2017-07-01 00:46:51 -0700

    reformat

--- bot.py
+++ bot.py
@@ -15,20 +15,22 @@ class Commands:
     @group.command(pass_context=True, no_pm=True)
     async def create(self, ctx, name):
         role = await create_role(self.bot, ctx.message.author.server, name)
-        await self.bot.add_roles(ctx.message.author,role)#add role to user
-        self.roles += role#add role to cache
+        await self.bot.add_roles(ctx.message.author, role)  # add role to user
+        self.roles += role  # add role to cache
 
         self.channels['text'] += await create_channel(self.bot, ctx.message.author.server, name)
 
-        vChannel = await create_channel(self.bot, ctx.message.author.server, name, True) 
-        await self.bot.move_member(ctx.message.author,vChannel)#move user to channel
-        self.channels['voice'] += vChannel#add role to cache
+        vChannel = await create_channel(self.bot, ctx.message.author.server, name, True)
+        await self.bot.move_member(ctx.message.author, vChannel)  # move user to channel
+        self.channels['voice'] += vChannel  # add role to cache
+
 
 async def create_role(client: Client, server: Server, name: str):
     uncached = await client.create_role(server, name=name)
-    return [r for r in server.roles if r.id == uncached.id]  # returns cached role 
+    return [r for r in server.roles if r.id == uncached.id]  # returns cached role
 
-async def create_channel(client: Client, server: Server, name: str, is_voice: bool=False):
+
+async def create_channel(client: Client, server: Server, name: str, is_voice: bool = False):
     channel_type = ChannelType.text
     if is_voice:
         channel_type = ChannelType.voice
