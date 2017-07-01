cdfc1b0509ea17547d4f11d3230dd87d597ae868
falconraptor <falconraptor@users.noreply.github.com>
2017-06-30 23:47:13 -0700

    added role and channel lists

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
 from discord.ext import commands
-
-from main import bot
 from discord import Server, Role
-
-
-@bot.event
-async def on_ready():
-    print('Logged in as: {0} (ID: {0.id})'.format(bot.user))
 
 
 class Commands:
     def __init__(self, bot):
         self.bot = bot
+        self.roles = []
+        self.channels = {'text': [], 'voice': []}
 
     @commands.group(name='group')
     async def group(self):
         pass
 
     @group.command(pass_context=True, no_pm=False)
-    async def create(self, ctx):
-        pass
+    async def create(self, ctx, name):
+        self.roles += create_role(ctx.message.author.server, name)
+        self.channels['text'] += create_channel(ctx.message.author.server, name)
+        self.channels['voice'] += create_channel(ctx.message.author.server, name, True)
 
 
 def create_role(server: Server, name: str):
