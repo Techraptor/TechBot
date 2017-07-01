8075e54186c794f347edc25fdcda3e1b45c26e7a
Marcus Kertesz <ModernMAK@Hotmail.com>
2017-07-01 00:15:03 -0700

    fixed idiotic mistake
    replaced create_role/channel with actual role/channel creation methods

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import Server, Role, ChannelType, Channel
+from discord import Server, Role, ChannelType, Channel, Client
 
 
 class Commands:
@@ -19,45 +19,51 @@ class Commands:
         self.channels['voice'] += create_channel(ctx.message.author.server, name, True)
 
 
-def create_role(server: Server, name: str):
-    def get_id():
-        id = 4
-        flag = True
-        while flag:
-            flag = False
-            for role in server.roles:
-                if not flag and role.id == id:
-                    flag = True
-                    id += 1
+async def create_role(client:Client, server: Server, name: str):
+    await client.create_role(server,{'name':name})
+    #def get_id():
+    #    id = 4
+    #    flag = True
+    #    while flag:
+    #        flag = False
+    #        for role in server.roles:
+    #            if not flag and role.id == id:
+    #                flag = True
+    #                id += 1
 
-    args = {'id': get_id(), 'server': server, 'name': name}
+    #args = {'id': get_id(), 'server': server, 'name': name}
 
-    return Role(**args)
-
-
-def create_channel(server: Server, name: str, is_voice: bool = False):
-    def get_id():
-        id = 4
-        flag = True
-        while flag:
-            flag = False
-            for channel in server.channel:
-                if not flag and channel.id == id:
-                    flag = True
-                    id += 1
-
-    args = {'id': get_id(), 'server': server}
-
-    # todo, fix name (lowercase and underscores)
-    args['name'] = name
-    args['topic'] = None
-    args['position'] = -1
+    #return Role(**args)
+async def create_channel(server: Server, name: str, is_voice: bool=False):
+    
+    channel_type = ChannelType.text
     if is_voice:
-        args['bitrate'] = 64
-        args['type'] = ChannelType.voice
-    else:
-        args['bitrate'] = 0
-        args['type'] = ChannelType.text
-    args['user_limit'] = 0
+        channel_type = ChannelType.voice
+    await client.create_channel(server,name,type=channel_type)
+    #def get_id():
+    #    id = 4
+    #    flag = True
+    #    while flag:
+    #        flag = False
+    #        for channel in server.channel:
+    #            if not flag and channel.id == id:
+    #                flag = True
+    #                id += 1
+                        
+    #def create_channel_info():
+    #    args = {'id': get_id(), 'server': server}
 
-    return Channel(**args)
+    #    # todo, fix name (lowercase and underscores)
+    #    args['name'] = name
+    #    args['topic'] = None
+    #    args['position'] = -1
+    #    if is_voice:
+    #        args['bitrate'] = 64
+    #        args['type'] = ChannelType.voice
+    #    else:
+    #        args['bitrate'] = 0
+    #        args['type'] = ChannelType.text
+    #    args['user_limit'] = 0
+
+    #    return Channel(**args)
+
